951ce6628abf50ecd35d22a6692707e6704c4854
BalajiR025 <85596957+BalajiR025@users.noreply.github.com>
2021-06-26 13:02:53 +0100

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript camp</h1>
+      <p>
+        This is a JavaScript and Reactjs bootcamp for absolute begineer by
+        sharyua singha sir thank you so much for that sir best camp I have ever
+        had
+      </p>
+    </div>
+  );
+}
+
+export default Info;
